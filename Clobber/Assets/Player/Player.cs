ae12301649ea1fea31edf59ef8658e7121adf320
Nate Hodge <hodge.nate@gmail.com>
2018-08-25 13:00:56 -0400

    Added a test player object with Twin Stick Movement.

--- Clobber/Assets/Player/Player.cs
+++ Clobber/Assets/Player/Player.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Player : MonoBehaviour
 {
+
 	// Use this for initialization
 	void Start ()
 	{
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		
+
 	}
 }
---
+++ Clobber/Assets/Player/TwinStickMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwinStickMovement : MonoBehaviour
+{
+	private Rigidbody2D rb;
+
+	public float moveSpeed = 2.0f;
+
+	// Use this for initialization
+	public void Start()
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	public void Update ()
+	{
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * moveSpeed;
+		transform.right = new Vector2(Input.GetAxis("RightHorizontal"), Input.GetAxis("RightVertical"));
+	}
+}
