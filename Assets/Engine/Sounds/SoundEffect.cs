7076b01644a954ec596b397d5bed13dd55a7e3ba
JFC <jcieszynski@gmail.com>
2018-09-11 21:42:19 -0400

    -Fixed the SoundEffect not playing any sound (timeLeftAlive was never set to anything above zero). -Added the volume and pitch modifier code back in (tots commented it out while trying to debug sound)

--- Assets/Engine/Sounds/SoundEffect.cs
+++ Assets/Engine/Sounds/SoundEffect.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class SoundEffect : MonoBehaviour
 {
-	private float timeLeftAlive = 0.0f;
+	private float timeLeftAlive = 1.0f;
 
 	// Use this for initialization
 	public void PlaySoundDefinition(SoundDefinition sound)
@@ -18,12 +18,13 @@ public class SoundEffect : MonoBehaviour
 		float globalVolumeSettingsPlaceholder = 1.0f; //TODO: once we have a settings dialog I think we'll want to replace this with the global volume setting - NOTE: may want to handle this via events instead
 
 		//NOTE - feel free to find a better algorithm to vary volume and pitch. 
-		soundeffectSource.volume = sound.Volume * (globalVolumeSettingsPlaceholder);// + Random.Range(1.0f - sound.VolumeModifier, 0.5f + sound.VolumeModifier));
-		soundeffectSource.pitch = sound.Pitch * (globalVolumeSettingsPlaceholder);// + Random.Range(1.0f - sound.PitchModifier, 1.0f + sound.PitchModifier));
+		soundeffectSource.volume = sound.Volume * (globalVolumeSettingsPlaceholder + Random.Range(1.0f - sound.VolumeModifier, 0.5f + sound.VolumeModifier));
+		soundeffectSource.pitch = sound.Pitch * (globalVolumeSettingsPlaceholder + Random.Range(1.0f - sound.PitchModifier, 1.0f + sound.PitchModifier));
 
+		timeLeftAlive = sound.Clip.length;
 		soundeffectSource.Play();
 	}
-	
+
 	// Update is called once per frame
 	void Update ()
 	{
