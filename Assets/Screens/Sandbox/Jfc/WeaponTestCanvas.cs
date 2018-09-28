33c0631054631ccebcd03c1ba1f549b31004ab99
JFC <jcieszynski@gmail.com>
2018-09-27 21:51:34 -0400

    Created Machine Gun and Missile Launcher weapons -created MachineGun and MissileLauncher and their associated scriptable objects -added buttons to the WeaponTestCanvas for testing and tuning -added a few sprites to try for PlayerBulletSmall.  Went with something similar to what we had for our original PlayerBullet.  Tried the green tipped "spaceMissiles_01" sprite as well but didn’t like it as much for this weapon.

--- Assets/Screens/Sandbox/Jfc/WeaponTestCanvas.cs
+++ Assets/Screens/Sandbox/Jfc/WeaponTestCanvas.cs
@@ -6,11 +6,22 @@ public class WeaponTestCanvas : MonoBehaviour
 {
 	public Player player;
 	public ProjectileWeaponDefinition laserWeapon;
+	public ProjectileWeaponDefinition machineGun;
+	public ProjectileWeaponDefinition missileLauncher;
 
 	public void EquipLaserWeapon()
 	{
 		player.EquipProjectileWeapon(laserWeapon);
 	}
 
+	public void EquipMachineGun()
+	{
+		player.EquipProjectileWeapon(machineGun);
+	}
+
+	public void EquipMissileLauncher()
+	{
+		player.EquipProjectileWeapon(missileLauncher);
+	}
 }
 
