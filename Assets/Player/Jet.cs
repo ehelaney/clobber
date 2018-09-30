6b7bffebd494dcd870c0e3c4fa88bdae78592b4d
Nate Hodge <hodge.nate@gmail.com>
2018-09-30 16:42:16 -0400

    Decoupled player aiming from movement.  Added Jets to player object to visualize movement.

---
+++ Assets/Player/Jet.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Jet : MonoBehaviour
+{
+	public Sprite lowSprite;
+	public Sprite highSprite;
+
+	private SpriteRenderer spriteRenderer;
+
+	private void Start()
+	{
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	public void SetPower(float power)
+	{
+		if (power < .25f)
+		{
+			spriteRenderer.sprite = null;
+		}
+		else if (power < .75f)
+		{
+			spriteRenderer.sprite = lowSprite;
+		}
+		else //power > .75f
+		{
+			spriteRenderer.sprite = highSprite;
+		}
+	}
+}
