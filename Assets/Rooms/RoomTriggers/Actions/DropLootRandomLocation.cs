5e267ed72254bc520293109b9a4b86710eb3d6df
Nate Hodge <hodge.nate@gmail.com>
2018-10-09 19:24:53 -0400

    Added a Room Action to drop loot based on a condition.  Modified Rooms 1, 2, and 4 to drop loot upon room success and removed the specific space capsules in those rooms.

---
+++ Assets/Rooms/RoomTriggers/Actions/DropLootRandomLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDropLoot", menuName = "Room Action/Drop Loot Random Location")]
+public class DropLootRandomLocation : RoomAction
+{
+	public LootTableDefinition lootTable;
+
+	public override void Execute()
+	{
+		var floor = FindObjectOfType<FloorController>();
+		Vector3 worldPoint = floor.GetWorldCoordinatesOfPoint(Random.Range(2, 16), Random.Range(2, 6));
+		FindObjectOfType<LootSystem>().ExecuteLootTable(lootTable, worldPoint);
+	}
+}
+
---
+++ Assets/Rooms/RoomTriggers/Actions/DropLootSpecificLocation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDropLoot", menuName = "Room Action/Drop Loot Specific Location")]
+public class DropLootSpecificLocation : RoomAction
+{
+	public LootTableDefinition lootTable;
+	public Vector2Int tileLocation;
+
+	public override void Execute()
+	{
+		var floor = FindObjectOfType<FloorController>();
+		Vector3 worldPoint = floor.GetWorldCoordinatesOfPoint(tileLocation.x, tileLocation.y);
+		FindObjectOfType<LootSystem>().ExecuteLootTable(lootTable, worldPoint);
+	}
+}
